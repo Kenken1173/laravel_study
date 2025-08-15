b3cab6b27c106d00c1cde6f964c467c48a63293e
kenken <anken914@ezweb.ne.jp>
2025-08-15 16:50:34 +0900

    画像を挿入し、その下にキャプションを追加した。画像から関連ページに遷移できるようにした。

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html>
+<x-header>
     <head>
-        <title>私のプロフィール</title>
+        <title>最近のアクティビティ</title>
     </head>
     <body>
         <h1>私について</h1>
@@ -9,5 +8,17 @@
         <p>私の名前は{{$name ?? "名無し"}}です。</p>
         <p>私の年齢は{{$age ?? "年齢未詳"}}です。</p>
         <button onclick="location.href='/'">私の好きなものはこちらです</button>
+
+        <!-- 画像＋リンク＋キャプション -->
+        <div style="text-align: center;">
+            <figure style="display: inline-block;">
+                <a href="https://arxiv.org/abs/2507.12886" target="_blank">
+                    <img src="{{ asset('images/abst_image.png') }}" alt="サンプル画像" style="max-width:300px; border-radius:8px;">
+                </a>
+                <figcaption style="margin-top:8px; color:#555;">
+                    クリックするとarXivに移動します。
+                </figcaption>
+            </figure>
+        </div>
     </body>
-</html>
+</x-header>
